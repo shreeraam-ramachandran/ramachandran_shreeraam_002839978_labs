efeaab729973e9b4e44e12a75b6db0f305cbd548
shreeraam-ramachandran <144941790+shreeraam-ramachandran@users.noreply.github.com>
2023-09-15 05:09:14 -0400

    Lab 1 - Simple Swing UI

---
+++ Lab 1/Lab_1/src/model/Product.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Home
+ */
+public class Product {
+    
+    private String name;
+    private String description;
+    private String availabilityNumber;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAvailabilityNumber() {
+        return availabilityNumber;
+    }
+
+    public void setAvailabilityNumber(String availabilityNumber) {
+        this.availabilityNumber = availabilityNumber;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    
+}
